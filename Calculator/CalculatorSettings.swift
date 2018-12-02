bb2f66cfb4579db566285f918c36474b2a557006
Zach Burt <zachary.burt05@gmail.com>
2018-12-02 12:03:26 -0500

    Update CalculatorSettings.swift

--- Calculator/CalculatorSettings.swift
+++ Calculator/CalculatorSettings.swift
@@ -5,6 +5,7 @@
 //  Created by Zach Burt on 11/26/18.
 //  Copyright © 2018 Zach Burt. All rights reserved.
 //
+//     Note of where comma's are placed in numbers.. 100  1,000  10,000  100,000  1,000,000  10,000,000  100,000,000  1,000,000,000
 
 import UIKit
 
@@ -15,10 +16,9 @@ class CalculatorSettings: UIViewController {
     }
     @IBOutlet weak var result: UILabel!
     
-    var storedResults = 0
-    var currentResults = 0
-    var solved = 0
-    var storedSign = 0
+    var storedResults = 0 // 1st part of equation is stored here
+    var currentResults = 0 // 2nd part of equation is stored here
+    var storedSign = 0 // equation sign (+/-/*/divide stored numerically here through their tags
     
     @IBAction func numbers(_ sender: UIButton) { //#'s 0-9
         
@@ -27,20 +27,19 @@ class CalculatorSettings: UIViewController {
         } else {
             result.text = result.text! + String(sender.tag)
         }
+        
         currentResults = Int(result.text!)!
-//     Note of where comma's are placed in numbers.. 100  1,000  10,000  100,000  1,000,000  10,000,000  100,000,000  1,000,000,000
     }
     
     @IBAction func equationSigns(_ sender: UIButton) { // 10: =, 11: +, 12: -, 13: *, 14: /, 15: +/-, 16: AC
+        
         if sender.tag == 16 { // AC - All Clear
-            result.text = "0"
-            storedResults = 0
-            currentResults = 0
-            storedSign = 0
+            clearAllResults()
             storeResults()
         } else if sender.tag == 10 { // =
             currentResults = Int(result.text!)!
             print("Stored: \(storedResults) , Current: \(currentResults)")
+            
             switch storedSign {
                 case 11: // +
                 result.text = String(storedResults + currentResults)
@@ -54,6 +53,7 @@ class CalculatorSettings: UIViewController {
                     result.text = String(storedResults + currentResults)
                     print("default called")
             }
+            
         } else if sender.tag == 11 || sender.tag == 12 || sender.tag == 13 || sender.tag == 14 { // Signs represented are +, -, *, /
             storeResults()
             result.text = "0"
@@ -68,5 +68,12 @@ class CalculatorSettings: UIViewController {
         } else {
             print("Can't store the results of a non-number")
         }
+    }
+    
+    func clearAllResults() {
+        result.text = "0"
+        storedResults = 0
+        currentResults = 0
+        storedSign = 0
     }
 }
